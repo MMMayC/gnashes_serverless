97ddb8b492d598cd77c4e4b6ad6e594dba0dad1e
Debanjan Basu <debanjanbasu2006@gmail.com>
2019-01-22 14:51:17 +1100

    updated to deploy to s3

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-  entry: {
-    // client: './src/client.js'
-    bundle: './express-api-endpoint/src/bundle.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'express-api-endpoint/public'),
-    // filename: "client.js"
-    filename: "bundle.js"
-  },
-  module:{
-    rules: [
-        {
-            test:/\.js$/,
-            exclude:/node_modules/,
-            loader: 'babel-loader'
-        },
-        {
-            test: /\.less$/,
-            use: [
-              {
-                loader: "style-loader"
-              },
-              {
-                loader: "css-loader",
-                options: {
-                  sourceMap: true,
-                  modules: true,
-                  localIdentName: "[local]"
-                }
-              },
-              {
-                loader: "less-loader"
-              }
-            ]
-        }
-    ]
-}
-}
+    entry: {
+        // client: './src/client.js'
+        bundle: "./spa/src/bundle.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "spa/public"),
+        // filename: "client.js"
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            modules: true,
+                            localIdentName: "[local]"
+                        }
+                    },
+                    {
+                        loader: "less-loader"
+                    }
+                ]
+            }
+        ]
+    }
+};
